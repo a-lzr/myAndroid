38785d0d39657e3c983d5b6a60347dc7fc6ba6a9
Alexander <developer.alexander.lazerko@gmail.com>
2020-03-10 21:15:57 +0300

    factoring

--- app/src/main/java/by/tms/myandroid/MainActivity.kt
+++ app/src/main/java/by/tms/myandroid/MainActivity.kt
@@ -3,30 +3,42 @@ package by.tms.myandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import by.tms.myandroid.lesson1.Lesson1Activity
 import by.tms.myandroid.lesson2.Lesson2Activity
 import by.tms.myandroid.lesson3.Lesson3Activity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startLesson1.setOnClickListener {
-            val intent = Intent(this, Lesson1Activity::class.java)
-            startActivity(intent)
-        }
+        startLesson1.setOnClickListener(this)
+        startLesson2.setOnClickListener(this)
+        startLesson3.setOnClickListener(this)
+        startLesson4.setOnClickListener(this)
+    }
 
-        startLesson2.setOnClickListener {
-            val intent = Intent(this, Lesson2Activity::class.java)
-            startActivity(intent)
-        }
-
-        startLesson3.setOnClickListener {
-            val intent = Intent(this, Lesson3Activity::class.java)
-            startActivity(intent)
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            startLesson1.id -> {
+                val intent = Intent(this, Lesson1Activity::class.java)
+                startActivity(intent)
+            }
+            startLesson2.id -> {
+                val intent = Intent(this, Lesson2Activity::class.java)
+                startActivity(intent)
+            }
+            startLesson3.id -> {
+                val intent = Intent(this, Lesson2Activity::class.java)
+                startActivity(intent)
+            }
+            startLesson4.id -> {
+                val intent = Intent(this, Lesson3Activity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
